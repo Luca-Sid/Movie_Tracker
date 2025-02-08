1bce96eb32c2599af0bc0cad082fb46447ecf3ce
Luca Sideri <luca.sideri@outlook.com>
2025-02-08 23:26:34 +0100

    Take keys as environment variables

---
+++ KEYS.py
@@ -0,0 +1,8 @@
+import os
+
+TMDB_API_KEY = 'bab238788c6abd18e527a0b64a65273f'
+
+# DB information
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
